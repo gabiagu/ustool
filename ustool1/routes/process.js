8f1871034a345ae82c7ad199f82a6eefcaa2f206
Gabriel Agu <agu.gabriel@gmail.com>
2016-12-11 09:55:31 +0100

    Revert "added initial files for ustool"
    This reverts commit 73c5e0fbb6637f12e0a88cd8b8e4853fd0b2dcdc.

--- ustool1/routes/process.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('badges:1,location:5,review:2');
-});
-
-module.exports = router;
